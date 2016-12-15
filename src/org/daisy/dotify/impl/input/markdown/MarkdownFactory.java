f7f8b5795abc9f2da870bc042c9a3dad8cee3e42
Joel Håkansson <joel.hakansson@mtm.se>
2016-12-15 14:32:07 +0100

    Updates version of dependencies

--- src/org/daisy/dotify/impl/input/markdown/MarkdownFactory.java
+++ src/org/daisy/dotify/impl/input/markdown/MarkdownFactory.java
@@ -3,25 +3,37 @@ package org.daisy.dotify.impl.input.markdown;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Locale;
+import java.util.Objects;
 import java.util.Set;
 
 import org.daisy.dotify.api.tasks.TaskGroup;
 import org.daisy.dotify.api.tasks.TaskGroupFactory;
+import org.daisy.dotify.api.tasks.TaskGroupInformation;
 import org.daisy.dotify.api.tasks.TaskGroupSpecification;
 
 public class MarkdownFactory implements TaskGroupFactory {
 	private final Set<TaskGroupSpecification> supportedSpecifications;
+	private final Set<TaskGroupInformation> information;
 	
 	public MarkdownFactory() {
 		this.supportedSpecifications = new HashSet<>();
 		for (Locale l : Locale.getAvailableLocales()) {
 			supportedSpecifications.add(new TaskGroupSpecification("md", "html", l.toLanguageTag()));
 		}
+		Set<TaskGroupInformation> tmp = new HashSet<>();
+		tmp.add(TaskGroupInformation.newConvertBuilder("md", "html").build());
+		information = Collections.unmodifiableSet(tmp);
 	}
 
 	@Override
 	public boolean supportsSpecification(TaskGroupSpecification specification) {
-		return supportedSpecifications.contains(specification);
+		//TODO: move this to default implementation after move to java 8
+		for (TaskGroupInformation i : listAll()) {
+			if (specification.matches(i)) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	@Override
@@ -30,12 +42,31 @@ public class MarkdownFactory implements TaskGroupFactory {
 	}
 
 	@Override
+	@Deprecated
 	public Set<TaskGroupSpecification> listSupportedSpecifications() {
 		return Collections.unmodifiableSet(supportedSpecifications);
 	}
 
 	@Override
 	public void setCreatedWithSPI() {
+	}
+
+	@Override
+	public Set<TaskGroupInformation> listAll() {
+		return information;
+	}
+
+	@Override
+	public Set<TaskGroupInformation> list(String locale) {
+		//TODO: move this to default implementation after move to java 8 (and use streams)
+		Objects.requireNonNull(locale);
+		Set<TaskGroupInformation> ret = new HashSet<>();
+		for (TaskGroupInformation info : listAll()) {
+			if (info.matchesLocale(locale)) {
+				ret.add(info.newCopyBuilder().locale(locale).build());
+			}
+		}
+		return ret;
 	}
 
 }
